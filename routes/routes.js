b3529b973296766a565e42a6cba5bb02b5f55477
namannangia <109794920+DevDah1ya@users.noreply.github.com>
2025-03-16 17:39:28 +0530

    refactor: format route definitions for better readability

--- routes/routes.js
+++ routes/routes.js
@@ -31,9 +31,15 @@ apiRoutes.post("/compare/product", controller.cron.compareProduct);
 apiRoutes.post("/createSmartList", controller.smartList.createSmartList);
 apiRoutes.put("/updateSmartList", controller.smartList.updateSmartList);
 apiRoutes.get("/smartlist", controller.smartList.smartList);
-apiRoutes.get("/getSmartList/:smartlist_id", controller.smartList.getSmartList)
-apiRoutes.get("/deleteSmartList/:smartlist_id", controller.smartList.deleteSmartList);
-apiRoutes.get("/getSmartListProducts/:smartlist_name", controller.smartList.getSmartListProducts)
+apiRoutes.get("/getSmartList/:smartlist_id", controller.smartList.getSmartList);
+apiRoutes.get(
+    "/deleteSmartList/:smartlist_id",
+    controller.smartList.deleteSmartList
+);
+apiRoutes.get(
+    "/getSmartListProducts/:smartlist_name",
+    controller.smartList.getSmartListProducts
+);
 
 ////apiRoutes.post('/subscription/notaccepted',   controller.cronjob.refundAmountToUser)
 ////apiRoutes.post('/refundamount',               controller.cronjob.refundAmountToUser);
@@ -407,8 +413,14 @@ apiRoutes.get(
     controller.deliveryBoy.bankdetails
 );
 apiRoutes.get("/deliveryboy/report", controller.deliveryBoy.report);
-apiRoutes.post("/deliveryboy/deductAmount", controller.deliveryBoy.deductAmount);
-apiRoutes.post("/deliveryboy/updateRentedBike", controller.deliveryBoy.updateRentedBike);
+apiRoutes.post(
+    "/deliveryboy/deductAmount",
+    controller.deliveryBoy.deductAmount
+);
+apiRoutes.post(
+    "/deliveryboy/updateRentedBike",
+    controller.deliveryBoy.updateRentedBike
+);
 apiRoutes.post("/importCsvAndUpdate", controller.product.importCsvAndUpdate);
 
 apiRoutes.post(
@@ -423,6 +435,6 @@ apiRoutes.post("/campaign/create", controller.campaigns.createCampaign);
 apiRoutes.post("/campaign/complete", controller.campaigns.completeCampaign);
 apiRoutes.post("/campaign/start", controller.campaigns.startCampaign);
 apiRoutes.post("/campaign/test", controller.campaigns.testCampaign);
-apiRoutes.put("/updateFeatureWall/:id", controller.HomeScreen.update);
-apiRoutes.get("/featureWall/:id", controller.HomeScreen.getFeatureWall);    
+// apiRoutes.put("/updateFeatureWall/:id", controller.HomeScreen.update);
+// apiRoutes.get("/featureWall/:id", controller.HomeScreen.getFeatureWall);
 module.exports = apiRoutes;
