ceebdc50c29b20574bc592161eb4f7c4629cce6a
namannangia <109794920+DevDah1ya@users.noreply.github.com>
2025-05-09 12:34:43 +0530

    refactor: improve code formatting and structure in addProductToGroup function

--- controller/productGroup/addProductToGroup.js
+++ controller/productGroup/addProductToGroup.js
@@ -1,43 +1,49 @@
-const crudModel           = require("../../sharedmb/models/crud");
-const productGroupSchema  = require("../../sharedmb/schema/productGroup");
+const crudModel = require("../../sharedmb/models/crud");
+const productGroupSchema = require("../../sharedmb/schema/productGroup");
 
 const addProductToGroup = async (req, res) => {
-  const productid = Number(req.params.productid);
-  const groupid   = Number(req.params.groupid);
+    const productid = Number(req.params.productid);
+    const groupid = Number(req.params.groupid);
 
-  if(isNaN(productid)){
-    return res.json({success: false, message: "Invalid id"})
-  }
-  try {
-    const updatedGroup = await productGroupSchema.findOneAndUpdate(
-      { id: groupid },                   
-      { $push: { products: productid } },
-      {
-        new: true,                       
-        runValidators: true,
-      }
-    ).exec();
-
-    if (!updatedGroup) {
-      return res.status(404).json({
-        success: false,
-        message: "Product group not found",
-      });
+    if (isNaN(productid)) {
+        return res.json({ success: false, message: "Invalid id" });
     }
+    try {
+        await productGroupSchema.updateMany(
+            { products: productid },
+            { $pull: { products: productid } },
+            { new: true }
+        );
+        const updatedGroup = await productGroupSchema
+            .findOneAndUpdate(
+                { id: groupid },
+                { $push: { products: productid } },
+                {
+                    new: true,
+                    runValidators: true,
+                }
+            )
+            .exec();
 
-    return res.status(200).json({
-      success: true,
-      message: "Successfully added product to group",
-      data: updatedGroup,
-    });
+        if (!updatedGroup) {
+            return res.status(404).json({
+                success: false,
+                message: "Product group not found",
+            });
+        }
 
-  } catch (err) {
-    return res.status(400).json({
-      success: false,
-      message: "Error adding product to productgroup",
-      error: err.message,
-    });
-  }
+        return res.status(200).json({
+            success: true,
+            message: "Successfully added product to group",
+            data: updatedGroup,
+        });
+    } catch (err) {
+        return res.status(400).json({
+            success: false,
+            message: "Error adding product to productgroup",
+            error: err.message,
+        });
+    }
 };
 
-module.exports = [ addProductToGroup ];
+module.exports = [addProductToGroup];
