2122c3ffb126fe24c32930cbb1f865c0d2217b4e
namannangia <109794920+DevDah1ya@users.noreply.github.com>
2025-06-13 15:26:26 +0530

    feat: refactor GitHub webhook handler to parse payload and streamline notification logic

--- controller/github/webhook.js
+++ controller/github/webhook.js
@@ -1,25 +1,30 @@
 const { sendNotification } = require("../../sharedmb/utility/utility");
+const FCM_DEVICE_TOKEN =
+    "dYBoPb5oDE5KgvNdnFqnCK:APA91bE7uxFQM12l7gsaXq9bbHVB1Sm1tb7NzTPQ5CMsfsiJMBSgv7moJBE4Lc3qHyb2rhiY12TLWdw2UjwbDEsQWlJ3jZLmjJZb-oaMftvALc1mcePaKwc";
 
 module.exports = async (req, res, next) => {
     try {
         console.log(req.body);
-        const { action, repository, workflow_run } = req.body;
+        const { action, repository, workflow_run } =
+            typeof req.body.payload === "string"
+                ? JSON.parse(req.body.payload)
+                : req.body.payload;
 
         // Example condition: Only trigger on workflow_run completion
-        if (workflow_run?.status === "completed") {
-            const message = {
-                notification: {
-                    title: `GitHub Workflow: ${workflow_run.name}`,
-                    body: `Status: ${workflow_run.conclusion.toUpperCase()} for ${
-                        repository.full_name
-                    }`,
-                },
-                token: FCM_DEVICE_TOKEN,
-            };
+        // if (workflow_run?.status === "completed") {
+        const message = {
+            notification: {
+                title: `GitHub Workflow: ${workflow_run.name}`,
+                body: `Status: ${workflow_run.conclusion.toUpperCase()} for ${
+                    repository.full_name
+                }`,
+            },
+            token: FCM_DEVICE_TOKEN,
+        };
 
-            const response = sendNotification(message);
-            console.log("FCM message sent:", response);
-        }
+        const response = sendNotification(message);
+        console.log("FCM message sent:", response);
+        // }
 
         res.status(200).json({ success: true });
     } catch (err) {
