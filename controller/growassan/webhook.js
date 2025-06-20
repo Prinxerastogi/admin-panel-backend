0172ae22bf9c15ab9a7caf2c5d4ad912516c7165
namannangia <109794920+DevDah1ya@users.noreply.github.com>
2025-06-20 12:30:37 +0530

    feat: implement growassan webhook handler and schema for processing incoming webhook data

---
+++ controller/growassan/webhook.js
@@ -0,0 +1,21 @@
+const webhookSchema = require("../../sharedmb/schema/webhook");
+
+const main = (req, res, next) => {
+    try {
+        const webhook = new webhookSchema({
+            data: req.body,
+        });
+
+        if (!webhook) {
+            throw Error("CANNOT SAVE WEBhook");
+        }
+        res.status(200).json({ success: true, message: "Recieved" });
+    } catch (err) {
+        console.error("error in growassan webhook", err);
+        return res
+            .status(500)
+            .json({ success: false, message: "INTERNAL SERVER ERROR" });
+    }
+};
+
+module.exports = main;
