2e440bdb30a04cbf0c9b3dc973a3dccd8a2ffebb
Naman Nangia <96681933+namannangia@users.noreply.github.com>
2025-07-05 11:06:53 +0530

    fix: remove unused ObjectId import from mongoose

--- controller/product/importCsvAndUpdate.js
+++ controller/product/importCsvAndUpdate.js
@@ -4,7 +4,6 @@ const multer = require("multer");
 // const sellerProductSchema = require("../../sharedmb/schema/sellerproduct");
 const productSchema = require("../../sharedmb/schema/product");
 const mongoose = require("mongoose");
-const { ObjectId } = mongoose.Types;
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
