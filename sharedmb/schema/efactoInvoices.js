6dade606c1c764e0cba475d6fd1f8a94122b3e54
Naman Nangia <96681933+namannangia@users.noreply.github.com>
2025-07-12 10:14:09 +0530

    number to string for invoice numebr

--- sharedmb/schema/efactoInvoices.js
+++ sharedmb/schema/efactoInvoices.js
@@ -1,7 +1,7 @@
 let mongoose = require("mongoose");
 
 let efactoInvoices = mongoose.Schema({
-    invoiceNo: { type: Number, default: null },
+    invoiceNo: { type: String, default: null },
     amount: { type: Number, default: null },
     date: { type: Date, default: null },
     phoneNo: { type: Number, default: null },
