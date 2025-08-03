eca8ca42a97110635ba523c9b4810f3b1acd2f9f
Naman Nangia <96681933+namannangia@users.noreply.github.com>
2025-08-03 18:15:31 +0530

    default status for webhooks

--- sharedmb/schema/webhook.js
+++ sharedmb/schema/webhook.js
@@ -1,9 +1,11 @@
 let mongoose = require("mongoose");
 let Schema = mongoose.Schema;
-
 const webhookSchema = new Schema({
-    data: {},
-    status: { type: String, enum: ["new", "begin", "err", "end"] },
+    data: Schema.Types.Mixed,
+    status: {
+        tiers: { type: String, default: "pending" },
+        sellerstock: { type: String, default: "pending" },
+    },
 });
 
 module.exports = mongoose.model("webhook", webhookSchema);
