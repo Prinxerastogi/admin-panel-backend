2782535256be7b72a1f3010274db2ecaccf3ce5a
Naman Nangia <96681933+namannangia@users.noreply.github.com>
2025-08-03 18:07:24 +0530

    addStatusToWebhooks

--- sharedmb/schema/webhook.js
+++ sharedmb/schema/webhook.js
@@ -3,6 +3,7 @@ let Schema = mongoose.Schema;
 
 const webhookSchema = new Schema({
     data: {},
+    status: { type: String, enum: ["new", "begin", "err", "end"] },
 });
 
 module.exports = mongoose.model("webhook", webhookSchema);
