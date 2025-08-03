85fc89b7464e80acc331b81cf7c9761dea236eec
Naman Nangia <96681933+namannangia@users.noreply.github.com>
2025-08-03 18:17:55 +0530

    add timestamp to webhooks

--- sharedmb/schema/webhook.js
+++ sharedmb/schema/webhook.js
@@ -1,11 +1,16 @@
 let mongoose = require("mongoose");
 let Schema = mongoose.Schema;
-const webhookSchema = new Schema({
-    data: Schema.Types.Mixed,
-    status: {
-        tiers: { type: String, default: "pending" },
-        sellerstock: { type: String, default: "pending" },
+const webhookSchema = new Schema(
+    {
+        data: Schema.Types.Mixed,
+        status: {
+            tiers: { type: String, default: "pending" },
+            sellerstock: { type: String, default: "pending" },
+        },
     },
-});
+    {
+        timestamps: true,
+    }
+);
 
 module.exports = mongoose.model("webhook", webhookSchema);
