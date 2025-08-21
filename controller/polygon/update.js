07091d1157c1ca4e27a6189eb4029621887deb9f
Naman Nangia <96681933+namannangia@users.noreply.github.com>
2025-08-21 17:12:38 +0530

    ensure loop closing when modified polygon

--- controller/polygon/update.js
+++ controller/polygon/update.js
@@ -4,7 +4,27 @@ const Polygon = require("../../sharedmb/schema/polygon");
 const updatePolygon = async (req, res) => {
     try {
         console.log("Updating polygon with ID:", req.params.id, req.body);
-        const updates = req.body || {};
+        let updates = req.body || {};
+
+        if (updates.geometry) {
+            let ring = req.body.geometry.coordinates[0] || [];
+
+            if (ring.length > 0) {
+                const [firstLng, firstLat] = ring[0];
+                const [lastLng, lastLat] = ring[ring.length - 1];
+                if (firstLng !== lastLng || firstLat !== lastLat) {
+                    ring.push([firstLng, firstLat]);
+                }
+            }
+
+            updates = {
+                ...updates,
+                geometry: {
+                    type: "Polygon",
+                    coordinates: [ring],
+                },
+            };
+        }
         const doc = await Polygon.findOneAndUpdate(
             { id: req.params.id },
             updates,
